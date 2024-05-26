0ddb97b995560a1803208173e5b51c4ba6fe12f8
ikhsolidum <100141624+ikhsolidum@users.noreply.github.com>
2024-05-27 06:59:53 +0800

    Update enrolled_courses_provider.dart

--- pages/enrolled_courses_provider.dart
+++ pages/enrolled_courses_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'course.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class EnrolledCoursesProvider extends ChangeNotifier {
   List<Course> _enrolledCourses = [];
@@ -15,10 +17,28 @@ class EnrolledCoursesProvider extends ChangeNotifier {
     notifyListeners();
   }
 
- void deleteCourse(String courseName) {
-    _enrolledCourses.removeWhere((course) => course.name == courseName);
-    notifyListeners();
+ void deleteCourse(String courseName) async {
+  final url = Uri.parse('https://ikhsolidum.helioho.st/courseconnect.php');
+
+  try {
+    final response = await http.delete(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'name': courseName}), 
+    );
+
+    if (response.statusCode == 200) {
+      _enrolledCourses.removeWhere((course) => course.name == courseName);
+      notifyListeners();
+    } else {
+      // Handle error
+      print('Failed to delete course: ${response.body}');
+    }
+  } catch (e) {
+    // Handle exception
+    print('Exception: $e');
   }
+}
 
   Map<String, dynamic> getCoursesData() {
     return {
